ee08bb9299455033a63a2fff03bec64ababd177c
vinvyach <vinvyach@yandex.ru>
2020-12-14 08:12:23 +0300

    style code

--- HiLo_game/src/ru/ru/ru/HiLo.java
+++ HiLo_game/src/ru/ru/ru/HiLo.java
@@ -6,40 +6,37 @@ public class HiLo {
 
 
     public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    String playAgain = new String();
-    String name;
-    int countTryInput = 0;
+        Scanner scan = new Scanner(System.in);
+        String playAgain = new String();
+        String name;
+        int countTryInput = 0;
 
         System.out.println("Input name: ");
         name = scan.nextLine();
+        do {
+            int theNumber = (int) (Math.random() * 200 - 100);
+            int guess = 0;
+            while (guess != theNumber) {
 
 
-    do{
-        int theNumber = (int) (Math.random()* 200 - 100);
-        int guess = 0;
-        while (guess != theNumber) {
+                System.out.println(name + " int the number -100 and 100");
+                guess = scan.nextInt();
+                countTryInput++;
 
+                System.out.println("You entered " + guess + ".");
+                if (guess < theNumber)
+                    System.out.println(guess + " is to low. Try again");
+                else if (guess > theNumber)
+                    System.out.println(guess + " is to high. Try again");
+                else
+                    System.out.println(guess + " is correct. You WIN!!!");
 
+            }
 
-            System.out.println(name + " int the number -100 and 100");
-            guess = scan.nextInt();
-            countTryInput++;
+            System.out.println(countTryInput + " Your count of try");
+            System.out.println("Would you like to play again (y/n)");
 
-            System.out.println("You entered " +guess + ".");
-            if (guess < theNumber)
-                System.out.println(guess + " is to low. Try again");
-            else if (guess > theNumber)
-                System.out.println(guess + " is to high. Try again");
-            else
-                System.out.println(guess + " is correct. You WIN!!!");
-
-        }
-
-        System.out.println(countTryInput + " Your count of try");
-        System.out.println("Would you like to play again (y/n)");
-
-        playAgain = scan.next();
+            playAgain = scan.next();
 
         } while (playAgain.equalsIgnoreCase("y"));
     }
