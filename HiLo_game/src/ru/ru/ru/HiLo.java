47d2a7120ff71c1dc292fc696c7136032ca995fd
vinvyach <vinvyach@yandex.ru>
2020-12-14 12:03:09 +0700

    small refactoring the game

--- HiLo_game/src/ru/ru/ru/HiLo.java
+++ HiLo_game/src/ru/ru/ru/HiLo.java
@@ -10,13 +10,17 @@ public class HiLo {
     String playAgain = new String();
     String name;
     int countTryInput = 0;
+
+        System.out.println("Input name: ");
+        name = scan.nextLine();
+
+
     do{
         int theNumber = (int) (Math.random()* 200 - 100);
         int guess = 0;
         while (guess != theNumber) {
 
-            System.out.println("Input name: ");
-            name = scan.nextLine();
+
 
             System.out.println(name + " int the number -100 and 100");
             guess = scan.nextInt();
@@ -29,8 +33,10 @@ public class HiLo {
                 System.out.println(guess + " is to high. Try again");
             else
                 System.out.println(guess + " is correct. You WIN!!!");
-            System.out.println(countTryInput + " Your count of try");
+
         }
+
+        System.out.println(countTryInput + " Your count of try");
         System.out.println("Would you like to play again (y/n)");
 
         playAgain = scan.next();
